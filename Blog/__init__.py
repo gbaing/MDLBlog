4e88a40ddcee64d338489b79184272af17137ecb
gbaing <gbaing@163.com>
2016-01-20 13:45:35 +0800

    Manage File Strcture

--- Blog/__init__.py
+++ Blog/__init__.py
@@ -1,3 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Create on 2016/1/20
+
+@Author:MinRui
+
+@Module Description:
+"""
 from flask import Flask
 
 app = Flask(__name__)
@@ -5,9 +13,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    print "test"
     return 'Hello World!'
 
+
+@app.route('/test1')
+def test01():
+    return "This is test1"
 
 if __name__ == '__main__':
     app.run()
---
+++ runserver.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Create on 2016/1/20
+
+@Author:MinRui
+
+@Module Description:
+"""
+from Blog import app
+app.run(debug=True)
